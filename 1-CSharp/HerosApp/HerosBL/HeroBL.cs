30b398893a0249a9c2602ec27d3e3e6eda67d447
angelagongli <angelagongli@gmail.com>
2020-10-26 21:32:46 -0500

    add HerosBL

---
+++ 1-CSharp/HerosApp/HerosBL/HeroBL.cs
@@ -0,0 +1,25 @@
+﻿using HerosLib;
+using HerosDB;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HerosBL
+{
+    public class HeroBL
+    {
+        IRepository repo = new FileRepo();
+        
+        public void AddHero(Hero newHero) {
+            // add business logic to check if hero name is unique for example
+            // throw exception otherwise
+            List<Hero> heroes = GetAllHeroes();
+            newHero.Id = heroes.Count + 1;
+            repo.AddHero(newHero);
+        }
+
+        public List<Hero> GetAllHeroes() {
+            Task<List<Hero>> getHeroes = repo.GetAllHeroes();
+            return getHeroes.Result;
+        }
+    }
+}
